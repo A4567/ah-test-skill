7f3a089797d9925f69f42e214b801369eb994a68
CTArchie <me@archiehollyfield.co.uk>
2020-05-27 12:37:24 +0100

    testing api

--- __init__.py
+++ __init__.py
@@ -1,13 +1,33 @@
 from mycroft import MycroftSkill, intent_file_handler
 
+import requests
+
+API_URL = 'https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=cycle-shops-and-repairs&q='
+
+def search_bike(region):
+    """Search openData for number of bike shops in region."""
+    r = requests.get(API_URL, params={'refine.region':region})
+    print('Searching in: ' + region)
+    
+    if(r.json()['nhits'] == 0):
+        return None
+    else:
+        return r.json()['nhits']
+
+
 
 class AhTest(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('Scream.intent')
-    def handle_test_ah(self, message):
-        self.speak_dialog('Scream')
+    @intent_file_handler('bike.intent')
+    def get_number_bikes(self, message):
+        number = search_bike(message.data['region'])
+        if number:
+            self.speak_dialog('bike',{'count':number})
+        else:
+            self.speak_dialog('Scream')
+        
 
 
 def create_skill():
