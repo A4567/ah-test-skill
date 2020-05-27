739c0936e70e2f185edd66fe874d4db48b7b7bbd
CTArchie <me@archiehollyfield.co.uk>
2020-05-27 11:56:46 +0100

    new intent/dialog

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ class AhTest(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('test.ah.intent')
+    @intent_file_handler('Scream.intent')
     def handle_test_ah(self, message):
-        self.speak_dialog('test.ah')
+        self.speak_dialog('Scream')
 
 
 def create_skill():
